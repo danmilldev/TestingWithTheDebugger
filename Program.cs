49737dc1fd24aa54bcd6c890a75ab47244637d44
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-03-31 16:58:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 using TestingWithTheDebugger;
 
 
-Debug.Assert(Calculator.Sum(5, 10) == 15, "The Sum Operation didnt gave the correct result");
+Debug.Assert(Calculator.Sum(5, 10) == 15, "The Sum Method didnt gave the correct result");
 
 Console.WriteLine("Sum: " + Calculator.Sum(5,10));
