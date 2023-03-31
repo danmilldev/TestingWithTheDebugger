113268ece9a5eed0c15077ffd8361fc7782a7cbf
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-03-31 17:07:43 +0200

    Made some Comments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.Diagnostics;
 using TestingWithTheDebugger;
 
-
+//Testing the sum method
 Debug.Assert(Calculator.Sum(5, 10) == 15, "The Sum Method didnt gave the correct result of 15.");
 
 Console.WriteLine("Sum of ( 5 , 10 ): " + Calculator.Sum(5,10));
 
+
+//Testing the subtract method
 Debug.Assert(Calculator.Subtract(20, 3) == 17, "The Subtract method didnt return the correct result of 17.");
 
 Console.WriteLine("Subtracted Number from ( 20 - 3 ): " + Calculator.Subtract(20,3));
